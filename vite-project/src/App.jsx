ad2e4461c3b6991b5d6c19a3611338d3bb868f3a
Siddharth Chaturvedi <chaturvedisiddharth11@gmail.com>
2025-05-15 09:59:39 +0530

    Map

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [show, setShow] = useState(true)
-
+  const [data,setData] = useState(null);
+  useEffect(()=>{
+    alert("UseEffect")
+  },[])
   return (
     <>
-      <div>
+      {/* <div>
         <button onClick={()=>{setShow(!show)}}>Toggle</button>
         {show? <div>Showing</div> : <div>Not Showing</div>}
-      </div>
+      </div> */}
+      {/* <div>Hey all</div> */}
+      
+      <ul>
+        {data && data.map(item=> <li key={item.id}>{item.name}</li>)}
+      </ul>
+
     </>
   )
 }
